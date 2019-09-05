c109f07099ccad62059e7fc843b6b09716b7dbd2
fooofei <aihujianfei@gmail.com>
2019-09-05 21:35:24 +0800

    add interior mut

---
+++ interior-mut-without-refcell/src/main.rs
@@ -0,0 +1,70 @@
+// A passed version of http://120.78.128.153/rustbook/ch15-05-interior-mutability.html
+
+pub trait Messenger {
+    fn send(&mut self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a mut T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T: Messenger> LimitTracker<'a, T> {
+    pub fn new(messenger: &'a mut T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+        let max_per = self.value as f64 / self.max as f64;
+
+        if max_per >= 0.75 && max_per < 0.9 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        } else if max_per >= 0.9 && max_per < 1.0 {
+            self.messenger
+                .send("warning: You've used up over 90% of your quota!");
+        } else if max_per >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockMessenger {
+        sent_messages: Vec<String>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: vec![],
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&mut self, message: &str) {
+            self.sent_messages.push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mut mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mut mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.len(), 1);
+    }
+}
+pub fn main() {}
---
+++ interior-mut/src/main.rs
@@ -0,0 +1,70 @@
+// from http://120.78.128.153/rustbook/ch15-05-interior-mutability.html
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T: Messenger> LimitTracker<'a, T> {
+    pub fn new(messenger: &'a T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+        let max_per = self.value as f64 / self.max as f64;
+
+        if max_per >= 0.75 && max_per < 0.9 {
+            self.messenger
+                .send("Warning: You've used up over 75% of your quota!");
+        } else if max_per >= 0.9 && max_per < 1.0 {
+            self.messenger
+                .send("warning: You've used up over 90% of your quota!");
+        } else if max_per >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+}
+pub fn main() {}
