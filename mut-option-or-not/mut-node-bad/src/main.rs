8f87bb6f1c861ccbc236d2e1287fd972e6b17236
fooofei <aihujianfei@gmail.com>
2019-09-11 20:30:29 +0800

    add mut option

---
+++ mut-option-or-not/mut-node-bad/src/main.rs
@@ -0,0 +1,68 @@
+#[derive(Debug)]
+struct Node {
+    s: String,
+    n: Option<Box<Node>>,
+}
+
+impl Node {
+    fn new(s :String) -> Self {
+        Self{
+            s:s,
+            n:None
+        }
+    }
+}
+
+// I know a way in https://rust-unofficial.github.io/too-many-lists/second-iter-mut.html
+// for differ with IterMut, we named NodeMutRef
+#[derive(Debug)]
+struct NodeMutRef<'a> {
+    n: &'a mut Option<Box<Node>>, //  I know Option<&'a mut Node> is ok, but why cannot this?
+}
+
+impl<'a> NodeMutRef<'a> {
+
+    fn new(l :&'a mut Option<Box<Node>>) -> Self {
+        Self{
+            n:l,
+        }
+    }
+
+    // 步骤拆分 // 仅仅看是否能够做到链表后移 // 做不到
+    fn test_move_next(&mut self) {
+        // `old_node` does not live long enough
+        let mut old_node = self.n.take();
+        old_node.as_mut().map(|e|{
+            self.n = &mut e.n;
+        });
+    }
+
+    // 步骤拆分 // 仅仅看是否能够返回可修改借用 // 是能够做到的
+    fn test_mut(&mut self) -> Option<&mut String> {
+        let r = self.n.as_mut().map(|e| {
+            &mut (**e).s
+        });
+        return r;
+    }
+}
+
+
+#[test]
+fn test1(){
+    let mut head = Node::new(String::from("aaa"));
+    let tail = Box::new(Node::new(String::from("bbb")));
+    head.n = Some(tail);
+
+    let mut iter = NodeMutRef::new(&mut head.n);
+    let mut s0 = iter.test_mut();
+    s0.map(|e| *e = format!("--{}",e));
+    assert_eq!(iter.test_mut(), Some(&mut String::from("--bbb")));
+
+    assert_eq!(iter.test_mut(), Some(&mut String::from("--bbb")));
+
+    iter.test_move_next();
+    assert_eq!(iter.test_mut(), None);
+}
+
+fn main() {
+}
---
+++ mut-option-or-not/mut-node-good/src/main.rs
@@ -0,0 +1,75 @@
+
+#[derive(Debug)]
+struct Node {
+    s: String,
+    n: Option<Box<Node>>,
+}
+
+impl Node {
+    fn new(s :String) -> Self {
+        Self{
+            s:s,
+            n:None
+        }
+    }
+}
+
+#[derive(Debug)]
+struct NodeMutRef<'a> {
+    n: Option<&'a mut Node>,
+}
+
+impl <'a> NodeMutRef<'a> {
+
+    fn new(l: &'a mut Node) -> Self{
+        Self{
+            n:Some(l),
+        }
+    }
+
+    // 步骤拆分 // 测试是否能够做到链表后移  // 竟然可以
+    fn test_move_next(&mut self) -> &mut Self {
+        self.n.take().map(|e|{
+            self.n = e.n.as_mut().map(|e0| {
+                // e0 = `&mut std::boxed::Box<Link>`
+                &mut (**e0)
+            });
+        });
+        self
+    }
+
+    // 步骤拆分 // 测试是否能够做到返回可修改借用 // 可以
+    fn test_mut(&mut self) -> Option<&mut String> {
+        self.n.as_mut().map(|e|{
+            //self.n = Some(e);
+            &mut e.s
+        })
+    }
+}
+
+
+#[test]
+fn test1(){
+    let mut head = Node::new(String::from("aaa"));
+    let tail = Box::new(Node::new(String::from("bbb")));
+    head.n = Some(tail);
+
+    let mut iter = NodeMutRef::new(&mut head);
+    let mut s0 = iter.test_mut();
+    s0.map(|e| *e = format!("--{}",e));
+    assert_eq!(iter.test_mut(), Some(&mut String::from("--aaa")));
+
+    assert_eq!(iter.test_mut(), Some(&mut String::from("--aaa")));
+
+
+    iter.test_move_next();
+    assert_eq!(iter.test_mut(), Some(&mut String::from("bbb")));
+
+    iter.test_move_next();
+    assert_eq!(iter.test_mut(), None);
+}
+
+fn main(){
+
+
+}
